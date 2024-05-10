5e4f3bb6ea5df47580f1383bd28767729a150382
Rushikesh Wakode <149246795+rushiwakode358@users.noreply.github.com>
2024-05-10 15:10:34 +0530

    Update and rename AllFetchStudent.java to AllStudentFetch.java

--- DDL/AllStudentFetch.java
+++ DDL/AllStudentFetch.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class AllFetchStudent {
+public class AllStudentFetch {
 
 	public static void main(String[] args) throws SQLException {
 
